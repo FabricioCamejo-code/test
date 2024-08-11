8f69b1f87c51e4afe140fae45eb8190a39f54e00
FabricioCamejo-code <fabry_18_rosariio@hotmail.com>
2024-08-10 21:36:54 -0300

    Nueva Actualizaciones npx nodemon

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-a = "hello :) new test";
-console.log(a)
+console.log("--------------------------------FabricioCamejo-code---------------------------------------")
+
+c = "hello :) new test";
+console.log(c)
+
+//-------------------------------FabricioCamejo-code--------------------------------------------------------
+console.log("--------------------------------FabricioCamejo-code---------------------------------------")
+
+function sumar(a,b){
+    return a + b
+}
+
+let n1 = 12 , n2 = 12
+
+console.log('La suma de  n1  mas  n2 es igual a: ',sumar(n1,n2))
+
+//-------------------------------FabricioCamejo-code---------------------------------------------------------
